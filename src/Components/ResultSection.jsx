054dd2edb829b2e207c78361883a3a109a4b6a97
LordAnthrax94 <andreagi1994@gmail.com>
2025-01-29 12:26:33 +0100

    fix: aggiunte bandierine per milestone 2

--- src/Components/ResultSection.jsx
+++ src/Components/ResultSection.jsx
@@ -1,24 +1,29 @@
+import languageFlag from "../assets/language"
+
 const ResultSection = ({title, data}) =>{
+
+  
 
   return (
     <div><h1>{title}</h1>
-             
-          {data.map((movie) => (<div className="col-12 col-md-4 m-3 d-flex justify-content-between flex-wrap" key={movie.id}>
-            
-            
-                <div className="card">
-                  <div className="card-body">
-                    <h3 className="card-text">{movie.title || movie.name}</h3>
-                  </div>            
-                  <ul className="list-group list-group-flush">
-                    <li className="list-group-item">{movie.original_title}</li>
-                    <li className="list-group-item">{movie.original_language}</li>
-                    <li className="list-group-item">{movie.vote_count}</li>
-                  </ul>
-                </div>
-                
+         <div className="d-flex justify-content-between flex-wrap">   
+            {data.map((movie) => (<div className="col-12 col-md-4 mt-2" key={movie.id}>
               
-          </div>))}
+              
+                  <div className="card" style={{width: "280px"}}>
+                    <div className="card-body">
+                      <h3 className="card-text">{movie.title || movie.name}</h3>
+                    </div>            
+                    <ul className="list-group list-group-flush">
+                      <li className="list-group-item">{movie.original_title || movie.original_name}</li>
+                      <li className="list-group-item">{movie.original_language} <img src={languageFlag[movie.original_language]} alt={movie.original_language} style={{width: "40px"}} /></li>
+                      <li className="list-group-item">{movie.vote_average}</li>
+                    </ul>
+                  </div>
+                  
+                
+            </div>))}
+          </div>
         </div>
     
     
